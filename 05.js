a90efd184768ff417d766850c50a0468b407af6d
prasetyo-adi <52805350+prasetyo-adi@users.noreply.github.com>
2019-07-20 22:40:16 +0700

    Add files via upload

---
+++ 05.js
@@ -0,0 +1,21 @@
+//penjumlahan Fibonacci genap
+
+function penjumlahanFibonacci(a,b){
+	return a + b ;
+}
+var a = parseInt(prompt('masukkan bilangan fibonacci genap (2,8,34) !:'));
+var b = parseInt(prompt('masukkan bilangan fibonacci genap (2,8,34) lagi !:'));
+var hasil = penjumlahanFibonacci(a,b);
+
+console.log(hasil)
+
+//penjumlahan Fibonacci ganjil
+
+function penjumlahanFibonacci(a,b){
+	return a + b ;
+}
+var a = parseInt(prompt('masukkan bilangan fibonacci ganjil (1,3,5,13,21,55,89)!:'));
+var b = parseInt(prompt('masukkan bilangan fibonacci ganjil (1,3,5,13,21,55,89) lagi !:'));
+var hasil = penjumlahanFibonacci(a,b);
+
+console.log(hasil);
